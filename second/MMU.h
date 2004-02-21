b08f2f15258bdad6c956754c14ee4cb0f3b61211
Laurent Vivier <Laurent@lvivier.info>
2004-02-21 01:52:51 +0000

    Add MMU_set_trace()

--- second/MMU.h
+++ second/MMU.h
@@ -11,6 +11,7 @@ void get_SRP(unsigned long *SRP);
 void get_CRP(unsigned long *CRP);
 void get_TT0(unsigned long *TT0);
 void get_TT1(unsigned long *TT1);
+int ptest(unsigned long logical, unsigned long* physical);
 unsigned long read_phys(unsigned long addr);
 void write_phys(void *addr, unsigned long value);
 
@@ -19,6 +20,10 @@ void write_phys(void *addr, unsigned long value);
 extern unsigned long get_page_size(void);
 extern int logical2physicalAttr(unsigned long logical, unsigned long *physicalAddr, unsigned long *attr);
 extern int logical2physical(unsigned long logical, unsigned long *physicalAddr);
+#define TRACE_MMU
+#ifdef TRACE_MMU
+extern void MMU_set_trace(int enable);
+#endif
 #ifdef MMU_DUMP
 extern void dump_MMU_table();
 #endif
