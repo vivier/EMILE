ae643f5728b0d7bbc0eaa063dfc97317184db371
Laurent Vivier <Laurent@lvivier.info>
2004-02-24 22:30:14 +0000

    Add ReadLocation(MachineLocation)

--- second/glue.h
+++ second/glue.h
@@ -4,8 +4,22 @@
  *
  */
 
+struct MachineLocation
+{
+	long latitude;
+	long longitude;
+	union
+	{
+		char dlsDelta;
+		long gmtDelta;
+	} u;
+};
+typedef struct MachineLocation MachineLocation;
+
+
 extern void glue_display_properties(unsigned long *base, 
 				    unsigned long *row_bytes,
 				    unsigned long *width, unsigned long *height,
 				    unsigned long *depth, unsigned long *video);
 extern int Gestalt(unsigned long selector, long * response);
+extern void ReadLocation(MachineLocation * loc);
