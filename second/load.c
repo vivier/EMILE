4e162a6fd7d0e1edda87cd3a2099ef8ffa9db978
Laurent Vivier <Laurent@lvivier.info>
2004-05-26 21:59:06 +0000

    load binary image from disk

---
+++ second/load.c
@@ -0,0 +1,45 @@
+/*
+ *
+ * (c) 2004 Laurent Vivier <LaurentVivier@wanadoo.fr>
+ *
+ */
+
+#include <stdio.h>
+#include <malloc.h>
+#include "glue.h"
+#include "load.h"
+
+char* load_image(unsigned long offset, unsigned long size)
+{
+	int err;
+	char* image;
+	ParamBlockRec_t param_block;
+
+	if (size == 0)
+		return NULL;
+
+	image = malloc(size);
+	if (image == 0)
+	{
+		free(image);
+		return NULL;
+	}
+
+	memset(&param_block, 0, sizeof(param_block));
+
+	param_block.ioBuffer = (unsigned long)image;
+	param_block.ioVRefNum = 1;
+	param_block.ioRefNum = -5;
+	param_block.ioReqCount = size;
+	param_block.ioPosMode = fsFromStart;
+	param_block.ioPosOffset = offset;
+
+	err = PBReadSync(&param_block);
+	if (err != noErr)
+	{
+		free(image);
+		return NULL;
+	}
+
+	return image;
+}
---
+++ second/load.h
@@ -0,0 +1,7 @@
+/*
+ *
+ * (c) 2004 Laurent Vivier <LaurentVivier@wanadoo.fr>
+ *
+ */
+                                                                                
+extern char* load_image(unsigned long offset, unsigned long size);
