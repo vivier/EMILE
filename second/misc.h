632dcb29bbfb07ddecc576ca96a74fad3a2e06e2
Laurent Vivier <Laurent@lvivier.info>
2004-06-02 17:37:06 +0000

    define u_int8_t, int8_t, u_int16_t, int16_t, u_int32_t, int32_t for m68k

--- second/misc.h
+++ second/misc.h
@@ -4,5 +4,12 @@
  *
  */
 
+typedef unsigned char u_int8_t;
+typedef signed char int8_t;
+typedef unsigned short u_int16_t;
+typedef signed short int16_t;
+typedef unsigned int u_int32_t;
+typedef signed int int32_t;
+
 extern void memdump(unsigned char* addr, unsigned long size);
 extern void error(char *x);
