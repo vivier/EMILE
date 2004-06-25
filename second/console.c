e40235b0acdd88cd4f6acce9bc0c609949569f67
Laurent Vivier <Laurent@lvivier.info>
2004-06-25 11:15:16 +0000

    use console mask to enable/disable vga and serial

--- second/console.c
+++ second/console.c
@@ -7,17 +7,28 @@
 #include "console.h"
 
 #include "vga.h"
+#include "serial.h"
+
+static int vga_enabled = 0;
 
 void
 console_init(emile_l2_header_t* info)
 {
-	vga_init();
+	if (info->console_mask & STDOUT_VGA)
+	{
+		vga_init();
+		vga_enabled = 1;
+	}
+	if ( info->console_mask & (STDOUT_SERIAL0 | STDOUT_SERIAL1) )
+		serial_init(info);
 }
 
 void
 console_put(char c)
 {
-	vga_put(c);
+	if (vga_enabled)
+		vga_put(c);
+	serial_put(c);
 }
 
 void
