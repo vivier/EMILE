1abf16c567f6d8852012e9e8da88032d33d9cb1f
Laurent Vivier <Laurent@lvivier.info>
2004-12-03 00:19:02 +0000

    add scsi support to load_image()

--- second/load.c
+++ second/load.c
@@ -9,30 +9,50 @@
 #include "bank.h"
 #include "misc.h"
 #include "glue.h"
+#include "head.h"
 #include "load.h"
 
-char* load_image(int unit, unsigned long offset, unsigned long size)
+#ifdef SCSI_SUPPORT
+#include "scsi.h"
+static char* load_container(struct emile_container* container, char* image)
 {
+	char* base = image;
+	int target;
+	int i;
 	int err;
-	char* image;
-	ParamBlockRec_t param_block;
 
-	if (size == 0)
-		return NULL;
+	target = container->unit_id;
 
-	image = malloc_contiguous(size + 4);
-	if (image == 0)
+	i = 0;
+	while (container->blocks[i].count != 0)
 	{
-		free(image);
-		return NULL;
+		err = scsi_READ(target, container->blocks[i].offset,
+				container->blocks[i].count,
+				image,
+				container->block_size * container->blocks[i].count);
+		if (err != noErr)
+		{
+			free(image);
+			return NULL;
+		}
+
+		image += container->block_size * container->blocks[i].count;
+		i++;
 	}
 
-	image = (char*)(((unsigned long)image + 3) & 0xFFFFFFFC);
+	return base;
+}
+#else	/* SCSI_SUPPORT */
+
+static char* load_blocks(unsigned long offset, unsigned long size, char *image)
+{
+	int err;
+	ParamBlockRec_t param_block;
 
 	memset(&param_block, 0, sizeof(param_block));
 
 	param_block.ioBuffer = (unsigned long)image;
-	param_block.ioVRefNum = unit;
+	param_block.ioVRefNum = 1;
 	param_block.ioRefNum = -5;
 	param_block.ioReqCount = size;
 	param_block.ioPosMode = fsFromStart;
@@ -46,4 +66,28 @@ char* load_image(int unit, unsigned long offset, unsigned long size)
 	}
 
 	return image;
+}
+#endif /* SCSI_SUPPORT */
+
+char* load_image(unsigned long offset, unsigned long size)
+{
+	char* image;
+
+	if (size == 0)
+		return NULL;
+
+	image = malloc_contiguous(size + 4);
+	if (image == 0)
+	{
+		free(image);
+		return NULL;
+	}
+
+	image = (char*)(((unsigned long)image + 3) & 0xFFFFFFFC);
+
+#ifdef SCSI_SUPPORT
+	return load_container((struct emile_container*)offset, image);
+#else
+	return load_blocks(offset, size, image);
+#endif
 }
