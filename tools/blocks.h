3d6bc88376e2260fcb33c0bac67d5eb1cb2a4a0c
Laurent Vivier <Laurent@lvivier.info>
2004-12-03 00:35:33 +0000

    declare scsi_create_container() for blocks.c

---
+++ tools/blocks.h
@@ -0,0 +1,11 @@
+/*
+ *
+ * (c) 2004 Laurent Vivier <LaurentVivier@wanadoo.fr>
+ *
+ */
+
+#define SCSI_SUPPORT
+
+#include "../second/head.h"
+
+extern int scsi_create_container(int fd, struct emile_container* container);
