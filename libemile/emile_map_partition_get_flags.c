880dd1d282c1d635df70f18ea8bce5bad61c39da
Laurent Vivier <Laurent@lvivier.info>
2004-12-21 15:32:02 +0000

    First revision

---
+++ libemile/emile_map_partition_get_flags.c
@@ -0,0 +1,14 @@
+static __attribute__((used)) char* rcsid = "$CVSHeader$";
+/*
+ *
+ * (c) 2004 Laurent Vivier <LaurentVivier@wanadoo.fr>
+ *
+ */
+
+#include "partition.h"
+#include "libemile.h"
+
+int emile_map_partition_get_flags(emile_map_t *map)
+{
+	return map->partition.Sig;
+}
