3538ecd3c2bc5b8fcbc01bc9bd3e71af22320053
Laurent Vivier <Laurent@lvivier.info>
2004-12-21 20:05:31 +0000

    First revision

---
+++ libemile/emile_map_has_apple_driver.c
@@ -0,0 +1,40 @@
+static __attribute__((used)) char* rcsid = "$CVSHeader$";
+/*
+ *
+ * (c) 2004 Laurent Vivier <LaurentVivier@wanadoo.fr>
+ *
+ */
+
+/*
+ * See http://developer.apple.com/technotes/tn/tn1189.html
+ *
+ */
+
+#include <string.h>
+
+#include "partition.h"
+#include "libemile.h"
+
+int emile_map_has_apple_driver(emile_map_t *map)
+{
+	int block, size, type, part;
+	int i;
+	int ret;
+
+	for (i = 0; i < emile_map_get_driver_number(map); i++)
+	{
+		emile_map_get_driver_info(map, i, &block, &size, &type);
+
+		part = emile_map_seek_driver_partition(map, block);
+
+		ret = emile_map_read(map, part);
+		if (ret == -1)
+			return -1;
+
+		if (strncmp(map->partition.PartType, 
+			      APPLE_DRIVER, strlen(APPLE_DRIVER)) == 0)
+			return 1;
+	}
+
+	return 0;
+}
