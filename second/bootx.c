5efe9903c3bf6bf528893290de339f52e3b99fa2
Laurent Vivier <Laurent@lvivier.info>
2005-05-12 05:51:56 +0000

    First revision

---
+++ second/bootx.c
@@ -0,0 +1,14 @@
+/*
+ *
+ * (c) 2005 Laurent Vivier <LaurentVivier@wanadoo.fr>
+ *
+ *
+ */
+
+
+#include "bootx.h"
+
+void bootx_init(char* command_line, 
+		   char* ramdisk_start, unsigned long ramdisk_size)
+{
+}
