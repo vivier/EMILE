4505b72ea2624f467836c5f7359be0a0e0b84529
Laurent Vivier <Laurent@lvivier.info>
2005-11-12 19:13:52 +0000

    Add strcmp() and strncmp()

---
+++ libunix/strcmp.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+int strcmp(const char *__s1, const char *__s2)
+{
+	while (*__s1 && *__s1 == *__s2)
+	{
+		__s1++;
+		__s2++;
+	}
+
+	return (*__s1 - *__s2);;
+}
---
+++ libunix/strncmp.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+int strncmp(const char *__s1, const char *__s2, size_t __n)
+{
+	int tmp;
+	while (__n--)
+	{
+		tmp = *__s1 - *__s2;
+		if (tmp)
+			return tmp;
+		if (!*__s1)
+			break;
+		__s1++; __s2++;
+	}
+		
+	return 0;
+}
