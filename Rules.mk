1bf043e22d2110b38a2f45216fbdbcea281c5994
Laurent Vivier <Laurent@lvivier.info>
2005-11-16 13:57:01 +0000

    Move some generic Makefile parts

---
+++ Rules.mk
@@ -0,0 +1,34 @@
+OBJS	= $(patsubst %.S,%.o,$(SOURCES:.c=.o))
+
+MODULE ?= $(basename $(TOP))
+
+DISTFILES ?= $(SOURCES) $(HEADERS) $(MANPAGES) Makefile
+
+%.8: %.sgml
+	docbook-to-man $< > $@
+
+%.8.gz: %.8
+	gzip -9c $< > $@
+
+ifdef LIBRARY
+$(LIBRARY): $(OBJS)
+	$(AR) rc $@ $^
+endif
+
+dist:
+	for file in $(DISTFILES); do \
+		dir=$$(dirname $$file); \
+		if [ "$$dir" != "" ] ; then \
+			mkdir -p $(DISTDIR)/$(MODULE)/$$dir; \
+		fi; \
+		cp -p $$file $(DISTDIR)/$(MODULE)/$$file; \
+	done
+
+ifdef TARGET
+clean:
+	cd $(TARGET) && \
+	rm -f $(OBJS) $(PROGRAMS) $(LIBRARY) $(MANPAGES)
+else
+clean:
+	rm -f $(OBJS) $(PROGRAMS) $(LIBRARY) $(MANPAGES)
+endif
