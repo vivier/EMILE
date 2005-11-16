43c7d65e4f8f501922b1ad59bb6fe4e12d963879
Laurent Vivier <Laurent@lvivier.info>
2005-11-16 00:48:20 +0000

    move to zlib-1.2.3

--- second/uncompress.h
+++
@@ -1,9 +0,0 @@
-/*
- *
- * (c) 2004 Laurent Vivier <LaurentVivier@wanadoo.fr>
- *
- */
-
-typedef unsigned char (*uncompress_get_byte_t)(unsigned long offset);
-
-extern unsigned long uncompress(char* buf, uncompress_get_byte_t feeder);
