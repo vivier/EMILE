cc453825573c913a43f582363c031b9ececbff81
Laurent Vivier <Laurent@lvivier.info>
2005-11-17 06:54:13 +0000

    moved to libscsi

--- second/scsi.h
+++
@@ -1,9 +0,0 @@
-/*
- *
- * (c) 2004 Laurent Vivier <LaurentVivier@wanadoo.fr>
- *
- */
-                                                                                
-extern int scsi_INQUIRY(int target, char* buffer, size_t count);
-extern int scsi_READ(int target, unsigned long offset, unsigned short nb_blocks,
-		     char *buffer, int buffer_size);
