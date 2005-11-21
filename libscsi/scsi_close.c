64e1e16e136ea6cc0d1f3b5369e5c6266e27ee45
Laurent Vivier <Laurent@lvivier.info>
2005-11-21 21:55:36 +0000

    Add scsi_close() and scsi_open()

---
+++ libscsi/scsi_close.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+#include "libscsi.h"
+
+int scsi_close(scsi_device_t *device)
+{
+	if (device == NULL)
+		return -1;
+	free(device);
+	return 0;
+}
---
+++ libscsi/scsi_open.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+#include "libscsi.h"
+
+#define SECTOR_SIZE	(2048)
+
+scsi_device_t *scsi_open(int target)
+{
+	scsi_device_t *dev;
+
+	dev = (scsi_device_t *)malloc(sizeof(scsi_device_t));
+	if (dev == NULL)
+		return NULL;
+	dev->target = target;
+	dev->sector_size = SECTOR_SIZE;
+
+	return dev;
+}
