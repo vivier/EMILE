6ab4fcd8bc0238171f4593e8dc34ff77101b7148
Laurent Vivier <Laurent@lvivier.info>
2005-11-22 23:18:06 +0000

    add load_kernel()

--- second/load.h
+++ second/load.h
@@ -6,3 +6,4 @@
                                                                                 
 extern int load_image(unsigned long offset, unsigned long size, char *image);
 extern int load_gzip(unsigned long offset, unsigned long size, char *image);
+extern char* load_kernel(char* path, int bootstrap_size, unsigned long *base, unsigned long *entry, unsigned long *size);
