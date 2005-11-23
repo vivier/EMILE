38e161e94c38e6f75a9bc6df410238bddbaca7b4
Laurent Vivier <Laurent@lvivier.info>
2005-11-23 00:12:10 +0000

    Add load_ramdisk(), remove load_image() and load_gzip()

--- second/load.h
+++ second/load.h
@@ -4,6 +4,5 @@
  *
  */
                                                                                 
-extern int load_image(unsigned long offset, unsigned long size, char *image);
-extern int load_gzip(unsigned long offset, unsigned long size, char *image);
 extern char* load_kernel(char* path, int bootstrap_size, unsigned long *base, unsigned long *entry, unsigned long *size);
+extern char* load_ramdisk(char* path, int *ramdisk_size);
