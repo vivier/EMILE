ed8593760b3f1bf16ad0c6e8fb6eb1e4d20c0a38
Laurent Vivier <Laurent@lvivier.info>
2005-11-28 20:55:20 +0000

    Add new functions for EM06

---
+++ libemile/emile_second_get_property.c
@@ -0,0 +1,18 @@
+#include <string.h>
+
+#include "libemile.h"
+
+int emile_second_get_property(char *configuration, char *name, char *property)
+{
+	int index = 0;
+	char current_name[256];
+	while (1)
+	{
+		index = emile_second_get_next_property(configuration, index, current_name, property);
+		if (index == -1)
+			break;
+		if (strcmp(name, current_name) == 0)
+			return 0;
+	}
+	return -1;
+}
---
+++ libemile/emile_second_set_property.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "libemile.h"
+void emile_second_set_property(char *configuration, char *name, char *property)
+{
+	int index = 0;
+	int current = 0;
+	char current_name[256];
+	char current_property[256];
+
+	while (1)
+	{
+		index = emile_second_get_next_property(configuration, index, current_name, current_property);
+		if (index == -1)
+			break;
+		if (strcmp(name, current_name) != 0)
+		{
+			memcpy(configuration + current, current_name, strlen(current_name));
+			current += strlen(current_name);
+			configuration[current++] = ' ';
+			memcpy(configuration + current, current_property, strlen(current_property));
+			current += strlen(current_property);
+		}
+	}
+	if (configuration[current - 1] != '\n')
+		configuration[current - 1] = '\n';
+	sprintf(configuration + current, "%s %s", name, property);
+}
