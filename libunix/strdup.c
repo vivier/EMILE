7a9f9c00e756db05561d2feb222ab4b0316eeb50
Laurent Vivier <Laurent@lvivier.info>
2005-12-08 06:33:17 +0000

    add strdup()

---
+++ libunix/strdup.c
@@ -0,0 +1,20 @@
+/*
+ *
+ * (c) 2004,2005 Laurent Vivier <LaurentVivier@wanadoo.fr>
+ *
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+char *strdup (__const char *__restrict __src)
+{
+	char *__dest;
+
+	__dest = (char*)malloc(strlen(__src) + 1);
+	if (__dest == NULL)
+		return NULL;
+	strcpy(__dest, __src);
+
+	return __dest;
+}
