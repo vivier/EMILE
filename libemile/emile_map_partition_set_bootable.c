7e67a5fe437112a437f9bbe895b13a4364075b46
Laurent Vivier <Laurent@lvivier.info>
2006-05-30 16:21:40 +0000

    correctly call emile_map_partition_set_flags()

--- libemile/emile_map_partition_set_bootable.c
+++ libemile/emile_map_partition_set_bootable.c
@@ -20,7 +20,7 @@ int emile_map_partition_set_bootable(emile_map_t *map, int enable)
 				kPartitionAUXIsInUse | kPartitionAUXIsReadable | 
 				kPartitionAUXIsWriteable | kPartitionIsMountedAtStartup | 0x80);
 	else
-		emile_map_partition_set_flags(&map, 
+		emile_map_partition_set_flags(map, 
 			emile_map_partition_get_flags(map) & ~kPartitionIsMountedAtStartup);
 
 	return 0;
