1153cd593a54cd21e952185976623bb6bd46da4d
Laurent Vivier <Laurent@lvivier.info>
2006-11-01 17:42:23 +0000

    add REQUEST_SENSE

---
+++ libscsi/scsi_REQUEST_SENSE.c
@@ -0,0 +1,36 @@
+/*
+ *
+ * (c) 2004-2006 Laurent Vivier <Laurent@lvivier.info>
+ *
+ */
+
+#include <sys/types.h>
+
+#include <scsi/scsi.h>
+
+#include <macos/types.h>
+#include <macos/errors.h>
+
+#include "libscsi.h"
+
+int scsi_REQUEST_SENSE(int target, char *buffer, size_t count)
+{
+	char	cdb[6];
+	TIB_t	tib[2];
+
+	cdb[0] = REQUEST_SENSE;
+	cdb[1] = 0;
+	cdb[2] = 0;
+	cdb[3] = 0;
+	cdb[4] = count;
+	cdb[5] = 0;
+
+	tib[0].opcode = op_no_inc;
+	tib[0].param1 = (int)buffer;
+	tib[0].param2 = count;
+	tib[1].opcode = op_stop;
+	tib[1].param1 = 0;
+	tib[1].param2 = 0;
+
+	return scsi_command(target, cdb, 6, tib);
+}
