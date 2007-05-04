d356dd3a530760e2d09501420a901d956ed66383
Laurent Vivier <Laurent@lvivier.info>
2007-05-04 09:14:49 +0000

    Add GetCurrentMode()

--- libmacos/macos/video.h
+++ libmacos/macos/video.h
@@ -1,11 +1,16 @@
 /*
  *
- * (c) 2004,2005 Laurent Vivier <Laurent@lvivier.info>
+ * (c) 2004-2007 Laurent Vivier <Laurent@lvivier.info>
  *
  */
 
 #ifndef __MACOS_VIDEO_H__
 #define __MACOS_VIDEO_H__
+
+#include <string.h>
+#include <macos/files.h>
+#include <macos/devices.h>
+
 typedef struct VDFlagRec
 {
         char flag;
@@ -20,4 +25,30 @@ typedef struct VDParamBlock
         void* csParam;
 } VDParamBlock;
 typedef VDParamBlock *VDParamBlockPtr;
+
+typedef struct VDSwitchInfoRec {
+	unsigned short csMode;
+	unsigned long csData;
+	unsigned short csPage;
+	void*	csBaseAddr;
+	unsigned long csReserved;
+} VDSwitchInfoRec;
+
+enum {
+	cscGetCurMode	= 10,
+};
+
+static inline OSErr GetCurrentMode(short refNum, VDSwitchInfoRec *hwMode)
+{
+	CntrlParam	param;
+
+	memset(&param, 0, sizeof(param));
+	memset(hwMode, 0, sizeof(hwMode));
+
+	param.csCode = cscGetCurMode;
+	param.ioCRefNum = refNum;
+	*((VDSwitchInfoRec **)&param.csParam[0]) = hwMode;
+
+	return PBStatusSync((ParmBlkPtr)&param);
+}
 #endif /* __MACOS_VIDEO_H__ */
