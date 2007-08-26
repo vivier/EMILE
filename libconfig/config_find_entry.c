37d97283745df05adf76ecbfe3f66c63b0fbb153
Laurent Vivier <Laurent@lvivier.info>
2007-08-26 19:17:29 +0000

    check if property is NULLi, name must be not NULL

--- libconfig/config_find_entry.c
+++ libconfig/config_find_entry.c
@@ -16,6 +16,9 @@ int config_find_entry(char *configuration, char *name, char *property)
 	char current_name[256];
 	char current_property[256];
 
+	if (name == NULL)
+		return -1;
+
 	while (configuration[index])
 	{
 		last_index = index;
@@ -24,8 +27,9 @@ int config_find_entry(char *configuration, char *name, char *property)
 		if (index == -1)
 			return -1;
 		if ( (strcmp(name, current_name) == 0) && 
-		     (strcmp(property, current_property) == 0) )
-			return index;
+		     ((property == NULL) || 
+		     		(strcmp(property, current_property) == 0)))
+			return last_index;
 		
 	}
 	return -1;
