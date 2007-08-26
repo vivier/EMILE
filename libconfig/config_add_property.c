027eced2d7a4601e441b3059a585650c014cd89b
Laurent Vivier <Laurent@lvivier.info>
2007-08-26 20:10:17 +0000

    add property at end

---
+++ libconfig/config_add_property.c
@@ -0,0 +1,23 @@
+/*
+ *
+ * (c) 2007 Laurent Vivier <Laurent@lvivier.info>
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "libconfig.h"
+
+void config_add_property(char* configuration, char* name, char* property)
+{
+	int index = strlen(configuration);
+
+	if (index > 0)
+	{
+		configuration[index] = '\n';
+		index++;
+	}
+	sprintf(configuration + index, 
+		"%s %s", name, property);
+}
