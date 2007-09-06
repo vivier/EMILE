33f08f27fcaf9510dbfdf92d51d1df8896213b76
Laurent Vivier <Laurent@lvivier.info>
2007-09-06 21:36:39 +0000

    detect the architecture of the kernel

---
+++ kernel.mk
@@ -0,0 +1,36 @@
+#
+# (c) 2004-2007 Laurent Vivier <Laurent@lvivier.info>
+#
+
+# Kernel architecture
+
+LINUX=$(shell ls $(LINUXPATH) 2> /dev/null)
+
+ifeq ($(LINUX),$(LINUXPATH))
+        FILEARCH=$(shell file -bknL $(LINUX) | cut -d, -f 2)
+        ifeq ($(findstring PowerPC, $(FILEARCH)), PowerPC)
+
+                KARCH=ppc
+                KSTRIP=$(PPC_STRIP)
+
+        else
+        ifeq ($(findstring Motorola 68000, $(FILEARCH)), Motorola 68000)
+
+                KARCH=classic
+                KSTRIP=$(M68K_STRIP)
+
+        else
+        ifeq ($(findstring Motorola 68, $(FILEARCH)), Motorola 68)
+
+                KARCH=m68k
+                KSTRIP=$(M68K_STRIP)
+        else
+                KARCH=unknown
+                KSTRIP=$(M68K_STRIP)
+        endif
+        endif
+        endif
+else
+        KARCH=m68k
+        KSTRIP=$(M68K_STRIP)
+endif
