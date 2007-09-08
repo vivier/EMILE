c4b4ee30ac4d791e99a97f27135e67625f1dc187
Laurent Vivier <Laurent@lvivier.info>
2007-09-08 22:07:04 +0000

    Add default values for LINUXPATH and LINUXRAMDISK

--- config.mk
+++ config.mk
@@ -19,3 +19,6 @@ CDBOOT_ARGS26="root=/dev/ram ramdisk_size=13000 $(CONSOLE)"
 INSTALLER_ARGS="prompt_ramdisk=1 load_ramdisk=1 ramdisk_start=0 root=/dev/fd0 ramdisk_size=4096 $(CONSOLE)"
 FLOPPY_CONF="floppy.conf"
 BOOT_ARGS="root=/dev/sda4 $(CONSOLE)"
+
+LINUXPATH=vmlinux
+LINUXRAMDISK=ramdisk.gz
