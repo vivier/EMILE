e8bbbe07bf547252c315ecea2993bf62d0338abc
Laurent Vivier <Laurent@lvivier.info>
2007-09-11 23:05:11 +0000

    config_read_vga() returns the value of the property, we select only one console if several are available (vga, modem, printer)

--- second/config.c
+++ second/config.c
@@ -22,6 +22,19 @@
 #include "misc.h"
 #include "bank.h"
 
+#define MSG_STATE_0 console_set_cursor_position(5, 1); 		\
+	printf("   Press 'b' or [RETURN] to boot or 'e' to edit\n");
+
+#define MSG_STATE_1 console_set_cursor_position(3, 1);		\
+	printf("   Press 'b' to boot, 'e' or [RETURN] to edit  \n"	\
+	       "   Press 'd' to delete, 'n' to create          \n"	\
+	       "   Press [ESC] to go back                      \n");
+
+#define MSG_CLEAN console_set_cursor_position(3, 1);		\
+	printf("                                               \n"	\
+	       "                                               \n"	\
+	       "                                               \n");
+
 #define MAX_KERNELS		20
 #define MAX_KERNEL_PARAMS	5
 
@@ -121,7 +134,7 @@ static char *decode_serial(char* s, int *baudrate, int *parity, int *datasize, i
 	return s;
 }
 
-int read_config_vga(emile_l2_header_t* info)
+char* read_config_vga(emile_l2_header_t* info)
 {
 	int8_t *configuration;
 	char property[64];
@@ -131,9 +144,9 @@ int read_config_vga(emile_l2_header_t* info)
 	ret = config_get_property(configuration, "vga", property);
 	close_config(configuration);
 	if (ret == -1)
-		return -1;
+		return NULL;
 
-	return 0;
+	return strdup(property);
 }
 
 int read_config_modem(emile_l2_header_t* info, int *bitrate, int *parity, int *datasize, int *stopbits)
@@ -195,14 +208,11 @@ int read_config(emile_l2_header_t* info,
 	int current;
 	int res;
 #if defined(USE_CLI) && defined(__LINUX__)
-	emile_window_t win = { 7, 4, 8, 72 };
+	int l, c;
+	emile_window_t win;
 	emile_list_t list;
 	int state;
 #endif
-
-	printf( "             EMILE v"VERSION
-		" (c) 2004-2007 Laurent Vivier (%ld kB)\n",
-		bank_mem_avail() / 1024);
 
 	if (!EMILE_COMPAT(EMILE_07_SIGNATURE, info->signature))
 	{
@@ -281,19 +291,25 @@ int read_config(emile_l2_header_t* info,
 	}
 	if (choice > index - 1)
 		choice = index - 1;
+
+#if defined(USE_CLI) && defined(__LINUX__)
+	if (console_select(timeout))
+		timeout = 0;
+
+	console_get_size(&l, &c);
+	console_clear();
+#endif
+	printf( "             EMILE v"VERSION
+		" (c) 2004-2007 Laurent Vivier (%ld kB)\n",
+		bank_mem_avail() / 1024);
+
 #if defined(USE_CLI) && defined(__LINUX__)
 	state = 0;
 
-#define MSG_STATE_0 console_set_cursor_position(5, 1); 		\
-	printf("   Press 'b' or [RETURN] to boot or 'e' to edit\n");
-#define MSG_STATE_1 console_set_cursor_position(3, 1);		\
-	printf("   Press 'b' to boot, 'e' or [RETURN] to edit  \n"	\
-	       "   Press 'd' to delete, 'n' to create          \n"	\
-	       "   Press [ESC] to go back                      \n");
-#define MSG_CLEAN console_set_cursor_position(3, 1);		\
-	printf("                                               \n"	\
-	       "                                               \n"	\
-	       "                                               \n");
+	win.l = 7;
+	win.c = 4;
+	win.h = l - 16;
+	win.w = c - 8;
 
 	MSG_STATE_0
 	while(state != -1)
--- second/config.h
+++ second/config.h
@@ -7,6 +7,6 @@
 #include "head.h"
 
 extern int read_config(emile_l2_header_t* info, char **kernel_path, char **command_line, char **ramdisk_path);
-extern int read_config_vga(emile_l2_header_t* info);
+extern char *read_config_vga(emile_l2_header_t* info);
 extern int read_config_modem(emile_l2_header_t* info, int *bitrate, int *parity, int *datasize, int *stopbits);
 extern int read_config_printer(emile_l2_header_t* info, int *bitrate, int *parity, int *datasize, int *stopbits);
