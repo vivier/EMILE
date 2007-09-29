2ebda90890faa52f14be685169ec5bfd5c3a6050
Laurent Vivier <Laurent@lvivier.info>
2007-09-29 21:27:48 +0000

    remove traces

--- libmacos/GetDCtlEntry.c
+++ libmacos/GetDCtlEntry.c
@@ -18,16 +18,13 @@ DCtlHandle GetDCtlEntry(short refNum)
 
 	count = LMGetUnitTableEntryCount();
 	currentHandle = (DCtlEntry ***) LMGetUTableBase();
-printf("\ndctlref ");
 	for (i = 0; i < count; i++)
 	{
 		if (!currentHandle[i])
 			continue;
 		currentPtr = *(currentHandle[i]);
-printf("%d \n", currentPtr->dCtlRefNum);
 		if (currentPtr->dCtlRefNum == refNum)
 			return currentHandle[i];
 	}
-printf("\n");
 	return (DCtlHandle)0;
 }
