636b4d086809ef48174e4e2b019fb3bc579d903b
Laurent Vivier <Laurent@lvivier.info>
2007-10-08 12:38:22 +0000

    Find scsi id accorind refnum and vice-versa

--- second/driver.h
+++ second/driver.h
@@ -1,7 +1,17 @@
 /*
  * 
- * (c) 2005 Laurent Vivier <Laurent@lvivier.info>
+ * (c) 2005-2007 Laurent Vivier <Laurent@lvivier.info>
  *
  */
+
+static inline int refnum_to_scsi_id(signed short refnum)
+{
+	return (~refnum) - 32;
+}
+
+static inline signed short scsi_id_to_refnum(int scsi_id)
+{
+	return ~((signed short)scsi_id + 32);
+}
 
 extern void turn_off_interrupts();
