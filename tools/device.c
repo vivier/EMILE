912c63ad481a5f979c9ff25d2b643ad7e1078a52
Laurent Vivier <Laurent@lvivier.info>
2007-10-10 16:46:03 +0000

    Add device_get_blocksize(), and adjust offset using blocksize instead of << 11

--- tools/device.c
+++ tools/device.c
@@ -16,14 +16,19 @@ int device_sector_size = 2048;
 
 #define BLOCKS(X)   (((X)/device_sector_size)+(((X)%device_sector_size)?1:0))
 
+int device_get_blocksize(void *data)
+{
+	return device_sector_size;
+}
+
 int device_read_sector(void *data,off_t offset, void* buffer, size_t size)
 {
 	int fd = (int)data;
 	int ret;
 
-	lseek(fd, offset << 11, SEEK_SET);
-	ret = read(fd, buffer, BLOCKS(size) << 11);
-	if (ret != BLOCKS(size) << 11)
+	lseek(fd, offset * device_sector_size, SEEK_SET);
+	ret = read(fd, buffer, BLOCKS(size) * device_sector_size);
+	if (ret != BLOCKS(size) * device_sector_size)
 		return -1;
 	return 0;
 }
@@ -33,9 +38,9 @@ int device_write_sector(void *data,off_t offset, void* buffer, size_t size)
 	int fd = (int)data;
 	int ret;
 
-	lseek(fd, offset << 11, SEEK_SET);
-	ret = write(fd, buffer, BLOCKS(size) << 11);
-	if (ret != BLOCKS(size) << 11)
+	lseek(fd, offset * device_sector_size, SEEK_SET);
+	ret = write(fd, buffer, BLOCKS(size) * device_sector_size);
+	if (ret != BLOCKS(size) * device_sector_size)
 		return -1;
 	return 0;
 }
