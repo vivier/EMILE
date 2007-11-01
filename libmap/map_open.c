3eb3d6005ef7aba727e6fdb50639f3640e822f3b
Laurent Vivier <Laurent@lvivier.info>
2007-11-01 22:52:17 +0000

    use directly block address instead of computing it with blocksize, correct parameter of get_blocksize()

--- libmap/map_open.c
+++ libmap/map_open.c
@@ -17,12 +17,18 @@ map_t* map_open(device_io_t *device)
 {
 	map_t *map;
 	int ret;
-	int blocksize = device->get_blocksize(device);
+	int blocksize = device->get_blocksize(device->data);
 
 	ASSERT_DD(printf("INTERNAL ERROR: Bad Block 0 size structure\n");
 		  return NULL;)
 	ASSERT_P(printf("INTERNAL ERROR: Bad Partition size structure\n"); 
 		 return NULL;)
+	
+	if (blocksize > MAP_BLOCKSIZE)
+	{
+		printf("device block size too big (%d)\n", blocksize);
+		return NULL;
+	}
 
 	map = (map_t*)malloc(sizeof(map_t));
 	if (map == NULL)
@@ -36,18 +42,20 @@ map_t* map_open(device_io_t *device)
 	}
 	memcpy(map->device, device, sizeof(device_io_t));
 
-	ret = device->read_sector(map->device->data, 0,
-				  &map->drivers, sizeof(map->drivers));
+	ret = map->device->read_sector(map->device->data, 0,
+				       &map->drivers, blocksize);
 	if (ret == -1)
 	{
+		printf("Cannot read block 0\n");
 		free(map);
 		return NULL;
 	}
 
-	ret = device->read_sector(map->device->data, sizeof(map->drivers) / blocksize,
-			       &map->partition, sizeof(map->partition));
+	ret = map->device->read_sector(map->device->data, 1,
+				       &map->partition, blocksize);
 	if (ret == -1)
 	{
+		printf("Cannot read first partition descriptor\n");
 		free(map);
 		return NULL;
 	}
