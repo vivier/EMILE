591dd8ba5ab2e88c05fad2a9b1a8310d59e437da
Laurent Vivier <laurent@vivier.eu>
2025-08-13 23:47:13 +0200

    container: add etch container
    To build inside an m68k/sid env:
    
      ./run-sid ./configure
      ./run-sid make all
    
    m68k/etch env:
    
      ./run-etch ./configure
      ./run-etch make all
    
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- container.mk
+++ container.mk
@@ -3,18 +3,44 @@
 # Container support
 
 CONTAINER_ENGINE ?= podman
-ROOTFS_IMG ?= localhost/m68k-sid-rootfs:latest
-EMILE_IMAGE ?= emile-m68k
 
-.PHONY: import-rootfs
-import-rootfs: .container/rootfs.tar.xz
-	@if $(CONTAINER_ENGINE) image exists "$(ROOTFS_IMG)"; then \
-		echo "$(ROOTFS_IMG) already present; skipping import"; \
+TAG_SID=20210211
+TAR_SID=rootfs-sid-$(TAG_SID).tar.xz
+ROOTFS_SID_IMG ?= m68k/sid/rootfs:$(TAG_SID)
+
+TAG_ETCH=latest
+TAR_ETCH=rootfs-etch-$(TAG_ETCH).tar.xz
+ROOTFS_ETCH_IMG ?= m68k/etch/rootfs:$(TAG_ETCH)
+
+EMILE_IMAGE_SID ?= emile-m68k-sid
+EMILE_IMAGE_ETCH ?= emile-m68k-etch
+
+.PHONY: import-sid-rootfs
+import-sid-rootfs: .container/$(TAR_SID)
+	@if $(CONTAINER_ENGINE) image exists "$(ROOTFS_SID_IMG)"; then \
+		echo "$(ROOTFS_SID_IMG) already present; skipping import"; \
 	else \
-		echo "Importing $(ROOTFS_IMG)"; \
-		$(CONTAINER_ENGINE) import .container/rootfs.tar.xz $(ROOTFS_IMG); \
+		echo "Importing $(ROOTFS_SID_IMG)"; \
+		$(CONTAINER_ENGINE) import --arch m68k --os debian --variant sid $< "$(ROOTFS_SID_IMG)"; \
 	fi
 
-.PHONY: emile-m68k-container
-emile-m68k-container: import-rootfs
-	$(CONTAINER_ENGINE) build -f .container/Containerfile.m68k -t $(EMILE_IMAGE) --build-arg UID=$$(id -u) --build-arg GID=$$(id -g)
+.PHONY: import-etch-rootfs
+import-etch-rootfs: .container/$(TAR_ETCH)
+	@if $(CONTAINER_ENGINE) image exists "$(ROOTFS_ETCH_IMG)"; then \
+		echo "$(ROOTFS_ETCH_IMG) already present; skipping import"; \
+	else \
+		echo "Importing $(ROOTFS_ETCH_IMG)"; \
+		$(CONTAINER_ENGINE) import --arch m68k --os debian --variant etch $< "$(ROOTFS_ETCH_IMG)"; \
+	fi
+
+
+.PHONY: emile-m68k-sid-container
+emile-m68k-sid-container: import-sid-rootfs
+	$(CONTAINER_ENGINE) build --pull=never -f .container/Containerfile-sid.m68k -t $(EMILE_IMAGE_SID) \
+				  --build-arg UID=$$(id -u) \
+				  --build-arg GID=$$(id -g)
+.PHONY: emile-m68k-etch-container
+emile-m68k-etch-container: import-etch-rootfs
+	$(CONTAINER_ENGINE) build --pull=never -f .container/Containerfile-etch.m68k -t $(EMILE_IMAGE_ETCH) \
+				  --build-arg UID=$$(id -u) \
+				  --build-arg GID=$$(id -g)
